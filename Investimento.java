d6283aac2e4529ac85fb6dea28349b8166697eb8
Leonardo Simon Monteiro <135035590+LeoSimonn@users.noreply.github.com>
2023-11-06 14:14:38 +0000

    Refactor Investimento class to improve investment calculations

--- Investimento.java
+++ Investimento.java
@@ -1,74 +1,119 @@
-public class Investimento{
+import java.util.Scanner;
 
-    private double saldoInicial;
+public class Investimento {
+    // Atributos da classe Investimento
+    private double saldo;
     private String nome;
-    private String tipo;
-    private int prazo;
-    private double aporte;
-    private double txmRendimento;
-    private double ctmRendimento;
+    private String tipo; // acao, tesouro, poupanca
+    private int prazo; // em meses
+    private double taxaRendimentoMensal; // em porcentagem
+    private double custoManutencao; // em porcentagem do lucro obtido
 
-
-    public Investimento(double saldoInicial, String nome, String tipo, int prazo){
-        this.saldoInicial = saldoInicial;
+    // Construtor
+    public Investimento(double saldo, String nome, String tipo, int prazo) {
+        this.saldo = saldo;
         this.nome = nome;
         this.tipo = tipo;
         this.prazo = prazo;
-
     }
 
-    public double getSaldoInicial(){
-        return this.saldoInicial;
+    // Métodos Getters e Setters
+    public double getSaldo() {
+        return saldo;
     }
 
-    public void depositar(double valor){
-        this.saldoInicial += valor;
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
     }
 
-    public double sacar(double valor){
-        if(valor > this.saldoInicial){
-            System.out.println("Saldo insuficiente");
-            return 0;
-        }else{
-            this.saldoInicial -= valor;
-            return valor;
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getPrazo() {
+        return prazo;
+    }
+
+    public void setPrazo(int prazo) {
+        this.prazo = prazo;
+    }
+
+    public double getTaxaRendimentoMensal() {
+        return taxaRendimentoMensal;
+    }
+
+    public double getCustoManutencao() {
+        return custoManutencao;
+    }
+
+    // Método para depositar um valor no saldo
+    public void depositar(double valor) {
+        this.saldo += valor;
+    }
+
+    // Método para sacar um valor do saldo
+    public boolean sacar(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return true; // Saque realizado com sucesso
+        } else {
+            System.out.println("Operação não realizada: Saldo insuficiente.");
+            return false; // Saque não realizado
         }
     }
 
-    public double preverRentabilidadeAoFinalDoPrazo(int prazo){
-        if(this.tipo.equals("Poupança")){
-            this.txmRendimento = 0.01;
-            this.ctmRendimento = 0.01;
-        }else if(this.tipo.equals("Ação")){
-            this.txmRendimento = 0.1;
-            this.ctmRendimento = 0.15;
-        }else if(this.tipo.equals("Tesouro")){
-            this.txmRendimento = 0.05;
-            this.ctmRendimento = 0.1;
+    // Método para escolher o tipo de investimento
+    public void escolherTipoInvestimento(String tipo) {
+        System.out.println("Os tipos de investimento disponíveis são: Ação, Tesouro e Poupança.");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o tipo de investimento: ");
+        tipo = scanner.nextLine();
+        if (tipo.equals("Ação")){
+            this.taxaRendimentoMensal = 10;
+            this.custoManutencao = 15;
+        } else if (tipo.equals("Tesouro")) {
+            this.taxaRendimentoMensal = 5;
+            this.custoManutencao = 10;
+        } else if (tipo.equals("Poupança")) {
+            this.taxaRendimentoMensal = 1;
+            this.custoManutencao = 1;
+        } else {
+            System.out.println("Tipo de investimento inválido.");
         }
-
-        return this.saldoInicial * Math.pow(1 + this.txmRendimento - this.ctmRendimento, prazo);
-
-        
-
     }
 
-    public double preverRentabilidadeAposPrazoComAporte(int prazo, double aporte){
-        if(this.tipo.equals("Poupança")){
-            this.txmRendimento = 0.01;
-            this.ctmRendimento = 0.01;
-        }else if(this.tipo.equals("Ação")){
-            this.txmRendimento = 0.1;
-            this.ctmRendimento = 0.15;
-        }else if(this.tipo.equals("Tesouro")){
-            this.txmRendimento = 0.05;
-            this.ctmRendimento = 0.1;
+    // Método para prever a rentabilidade ao final do prazo
+    public double preverRentabilidadeAoFinalDoPrazo() {
+        double saldoPrevisto = this.saldo;
+        for (int i = 0; i < this.prazo; i++) {
+            double rendimento = saldoPrevisto * (this.taxaRendimentoMensal / 100);
+            double custo = rendimento * (this.custoManutencao / 100);
+            saldoPrevisto += (rendimento - custo);
         }
-
-        return this.saldoInicial * Math.pow(1 + this.txmRendimento - this.ctmRendimento, prazo) + aporte * (Math.pow(1 + this.txmRendimento - this.ctmRendimento, prazo) - 1) / (this.txmRendimento - this.ctmRendimento);
-
+        return saldoPrevisto - this.saldo; // Retorna apenas o rendimento
     }
 
-    
-
+    // Método para prever a rentabilidade ao final do prazo considerando aporte mensal
+    public double preverRentabilidadeAoFinalDoPrazoComAporteMensal(double aporteMensal) {
+        double saldoPrevisto = this.saldo;
+        for (int i = 0; i < this.prazo; i++) {
+            saldoPrevisto += aporteMensal;
+            double rendimento = saldoPrevisto * (this.taxaRendimentoMensal / 100);
+            double custo = rendimento * (this.custoManutencao / 100);
+            saldoPrevisto += (rendimento - custo);
+        }
+        return saldoPrevisto - this.saldo; // Retorna apenas o rendimento
+    }
 }
--- Main.java
+++ Main.java
@@ -3,12 +3,14 @@ public class Main{
     public static void main(String[] args){
 
         Investimento investimento = new Investimento(1000, "Investimento 1", "Poupança", 12);
-
         investimento.depositar(1000);
-        investimento.sacar(500);
-        investimento.sacar(1000);
+        investimento.sacar(100);
+        investimento.escolherTipoInvestimento("Poupança");
+        investimento.preverRentabilidadeAoFinalDoPrazo();
+        investimento.preverRentabilidadeAoFinalDoPrazoComAporteMensal(500);
 
-        System.out.println(investimento.getSaldoInicial());
+        
+
 
 
      
